e328913ebc20f6947294492e8a58133b81c1865a
CodingDotExe <8ZkF0yNX@protonmail.com>
2020-03-06 16:35:23 +0100

    index.js
    Better code just by using some ternary.

--- changer.js
+++ changer.js
@@ -1,72 +1,10 @@
+exports.changer = function(house, token) {
+    const request = require("request")
 
-   exports.changer = function(house, token) {
-    const request = require("request");
-    
-        var selection = house
-        if(selection == "Bravery") {
-        request({
-            method: "POST",
-            url: "https://discordapp.com/api/v6/hypesquad/online",
-            body: {'house_id': 1},
-            json: true,
-            headers: {
-                authorization: token
-            }
-        }, function(error, response, body) {
-            if(error) console.log(error);
-            return console.log("Changed");
-        })
-    }
+    request({method: "POST",
+    url: "https://discordapp.com/api/v6/hypesquad/online",
+    body: { "house_id": house == "Bravery" ? 1 : house == "Brilliance" ? 2 : house == "Balance" ? 3 : undefined },
+    json: true,
+    headers: { "Authorization": token }}, (error, _, _) => { error ? console.log(error) : console.log("House changed !") })
 
-    if(selection == "Brilliance") {
-        request({
-            method: "POST",
-            url: "https://discordapp.com/api/v6/hypesquad/online",
-            body: {'house_id': 2},
-            json: true,
-            headers: {
-                authorization: token
-            }
-        }, function(error, response, body) {
-            if(error) console.log(error);
-            return console.log("Changed");
-        })
-    }
-
-    if(selection == "Balance") {
-        request({
-            method: "POST",
-            url: "https://discordapp.com/api/v6/hypesquad/online",
-            body: {'house_id': 3},
-            json: true,
-            headers: {
-                authorization: token
-            }
-        }, function(error, response, body) {
-            if(error) console.log(error);
-            return console.log("Changed");
-
-        })
-    }
-
-    if(selection == "Random") {
-        var houses = [1,2,3]
-        request({
-            method: "POST",
-            url: "https://discordapp.com/api/v6/hypesquad/online",
-            body: {'house_id': houses[Math.floor(Math.random() * houses.length)]},
-            json: true,
-            headers: {
-                authorization: token
-            }
-        }, function(error, response, body) {
-            if(error) console.log(error);
-            return console.log("Changed");
-
-        })
-    }
-         }
-        
-    
-
-
+}
