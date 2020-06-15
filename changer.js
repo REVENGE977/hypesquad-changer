3526ad8a4802f0767338387c38f9c9e446f0ed87
REVENGE <32039975+Bowlingtoolkit@users.noreply.github.com>
2020-06-15 18:41:12 +0300

    just changing the api route url
    https://discordapp.com/ domain soon will be no longer work ,
    so i have to change the api route url to https://discord.com/api/v6/hypesquad/online instead of https://discordapp.com/api/v6/hypesquad/online

--- changer.js
+++ changer.js
@@ -1,10 +1,11 @@
 exports.changer = function(house, token) {
-    const request = require("request")
-    var RandomSquad = [1,2,3]
-    request({method: "POST",
-    url: "https://discordapp.com/api/v6/hypesquad/online",
-    body: { "house_id": house == "Bravery" ? 1 : house == "Brilliance" ? 2 : house == "Balance" ? 3 : house == "Random" ? RandomSquad[Math.floor(Math.random() * RandomSquad.length)] : undefined },
-    json: true,
-    headers: { "Authorization": token }}, (error) => { error ? console.log(error) : console.log("House changed !") })
-
+    const request = require("request");
+    var RandomSquad = [1,2,3];
+    request({
+        method: "POST",
+        url: "https://discord.com/api/v6/hypesquad/online",
+        body: { "house_id": house == "Bravery" ? 1 : house == "Brilliance" ? 2 : house == "Balance" ? 3 : house == "Random" ? RandomSquad[Math.floor(Math.random() * RandomSquad.length)] : undefined },
+        json: true,
+        headers: { "Authorization": token }}, (error) => { error ? console.log(error) : console.log("House changed !") 
+    })
 }
