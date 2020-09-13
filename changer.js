ce654bf7f971d89a85821c030ab041879864ac58
REVENGE <32039975+KAMUI966@users.noreply.github.com>
2020-09-13 19:06:37 +0300

    Update changer.js

--- changer.js
+++ changer.js
@@ -3,7 +3,7 @@ exports.changer = function(house, token) {
     var RandomSquad = [1,2,3];
     request({
         method: "POST",
-        url: "https://discord.com/api/v6/hypesquad/online",
+        url: "https://discord.com/api/v8/hypesquad/online",
         body: { "house_id": house == "Bravery" ? 1 : house == "Brilliance" ? 2 : house == "Balance" ? 3 : house == "Random" ? RandomSquad[Math.floor(Math.random() * RandomSquad.length)] : undefined },
         json: true,
         headers: { "Authorization": token }}, (error) => { error ? console.log(error) : console.log("House changed !") 
