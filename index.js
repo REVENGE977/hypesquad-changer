991821a527003fd92247d5ec42b4cf5dc352dcad
quatrecentquatre-404 <obvious.ly.dev@gmail.com>
2021-05-15 14:13:59 +0200

    Token was in index.js, removed and refreshed.

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 const { handler } = require("./src/hypesquad")
-handler("Brillance", "mfa.1PBd1Sta04V7Te91BeY4xFZMBeCpTRTbfPwvamEjM1IHD77jckZ2D2C300RSr68k0uTK80ixoc_PkrYbkLUF")
+handler("Brillance", "TOKEN")
